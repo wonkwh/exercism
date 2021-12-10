37d75741828077c0ac458565e05bbb4f36ea6c10
kwanghyun.won <wonkwh@gmail.com>
2021-12-10 12:18:20 +0900

    typescript hello-world

---
+++ typescript/hello-world/hello-world.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.hello2 = exports.hello = void 0;
+function hello() {
+    return 'Hello, World!';
+}
+exports.hello = hello;
+function hello2() {
+    return 'hello2, world';
+}
+exports.hello2 = hello2;
